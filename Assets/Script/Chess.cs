a80891a1e1afd1f99f07995160b25509f51d5999
KIM JUN <972002a@naver.com>
2020-08-06 15:44:45 +0900

    chess drop and collision

---
+++ Assets/Script/Chess.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chess : MonoBehaviour
+{
+    public GameObject[] m_Particles;
+    // Start is called before the first frame update
+  
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Map")
+        {
+            int index = Random.Range(0, m_Particles.Length);
+           
+            Debug.Log(collision.contacts[0].point);
+
+            Vector3 vector = collision.contacts[0].point;
+           
+
+            Instantiate(m_Particles[index], vector, Quaternion.identity);
+            Destroy(this.gameObject);
+            
+        }
+        else if(collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+      
+    }
+}
---
+++ Assets/Script/ChessGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessGame : MonoBehaviour
+{
+    public GameObject[] m_spawnPosition;
+    public GameObject[] m_chess;
+    
+    void Start()
+    {
+        m_spawnPosition = GameObject.FindGameObjectsWithTag("ChessSpawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            int chessIndex = Random.Range(0, m_chess.Length);
+            int posIndex = Random.Range(0, m_spawnPosition.Length);
+
+            Debug.Log(chessIndex);
+            Debug.Log(posIndex);
+
+            Instantiate(m_chess[chessIndex], m_spawnPosition[posIndex].transform.position, Quaternion.identity);
+        }
+    }
+}
